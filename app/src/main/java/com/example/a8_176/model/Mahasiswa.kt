8bbcc141b50a24becf321da3e1fe04f5bda39c9d
riskalaluna <riskalaluna@gmail.com>
2025-01-21 17:52:04 +0700

    menambahkan data class AllMahasiswaResponse

--- app/src/main/java/com/example/a8_176/model/Mahasiswa.kt
+++ app/src/main/java/com/example/a8_176/model/Mahasiswa.kt
@@ -3,6 +3,13 @@ package com.example.a8_176.model
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class AllMahasiswaResponse (
+    val status: Boolean,
+    val message: String,
+    val data: List<Mahasiswa>
+)
+
+@Serializable
 data class Mahasiswa(
     val id_mahasiswa: String,
     val nama_mahasiswa: String,
