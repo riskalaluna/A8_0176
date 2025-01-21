23e58bed43606fdeb0cfebc7cec1215982205966
riskalaluna <riskalaluna@gmail.com>
2025-01-21 18:47:17 +0700

    Menambahkan data class AllBangunanResponse dan BangunanDetailResponse

--- app/src/main/java/com/example/a8_176/model/Bangunan.kt
+++ app/src/main/java/com/example/a8_176/model/Bangunan.kt
@@ -3,6 +3,20 @@ package com.example.a8_176.model
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class AllBangunanResponse (
+    val status: Boolean,
+    val message: String,
+    val data: List<Bangunan>
+)
+
+@Serializable
+data class BangunanDetailResponse (
+    val status: Boolean,
+    val message: String,
+    val data: Bangunan
+)
+
+@Serializable
 data class Bangunan(
     val id_bangunan: String,
     val nama_bangunan: String,
