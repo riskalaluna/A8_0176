1f067534e398ec4b2333b2f04e85b4515dd82237
riskalaluna <riskalaluna@gmail.com>
2025-01-21 17:40:33 +0700

    menambahkan data class Mahasiswa

---
+++ app/src/main/java/com/example/a8_176/model/Mahasiswa.kt
@@ -0,0 +1,13 @@
+package com.example.a8_176.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Mahasiswa(
+    val id_mahasiswa: String,
+    val nama_mahasiswa: String,
+    val nomor_identitas: String,
+    val email: String,
+    val nomor_telepon: String,
+    val id_kamar: String
+)
