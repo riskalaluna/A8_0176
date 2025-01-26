5c855661aa45a8d5706362c42375f77968531200
riskalaluna <riskalaluna@gmail.com>
2025-01-26 08:53:48 +0700

    menangani operasi insert data Bangunan

--- app/src/main/java/com/example/a8_176/ui/viewmodel/bangunan/InsertBangunanViewModel.kt
+++ app/src/main/java/com/example/a8_176/ui/viewmodel/bangunan/InsertBangunanViewModel.kt
@@ -1,6 +1,34 @@
 package com.example.a8_176.ui.viewmodel.bangunan
 
 import com.example.a8_176.model.Bangunan
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a8_176.repository.BangunanRepository
+import kotlinx.coroutines.launch
+
+class InsertBangunanViewModel(
+    private val bgn: BangunanRepository
+): ViewModel() {
+    var uiState by mutableStateOf(InsertBgnUiState())
+        private set
+
+    fun updateInsertBgnState(insertBgnUiEvent: InsertBgnUiEvent){
+        uiState = InsertBgnUiState(insertBgnUiEvent = insertBgnUiEvent)
+    }
+
+    fun insertBgn() {
+        viewModelScope.launch {
+            try {
+                bgn.insertBangunan(uiState.insertBgnUiEvent.toBgn())
+            } catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class InsertBgnUiState(
     val insertBgnUiEvent: InsertBgnUiEvent = InsertBgnUiEvent()
@@ -28,3 +56,4 @@ fun Bangunan.toInsertBgnUiEvent(): InsertBgnUiEvent = InsertBgnUiEvent(
     jumlah_lantai = jumlah_lantai,
     alamat = alamat
 )
+
