0b995baead79a357612703465450767c3b2ec0a7
riskalaluna <riskalaluna@gmail.com>
2025-01-26 21:30:43 +0700

    menyesuaikan code agar bisa onBack

--- app/src/main/java/com/example/a8_176/ui/custumwidget/TopAppBar.kt
+++ app/src/main/java/com/example/a8_176/ui/custumwidget/TopAppBar.kt
@@ -1,6 +1,5 @@
 package com.example.a8_176.ui.custumwidget
 
-import android.icu.text.CaseMap.Title
 import androidx.compose.foundation.clickable
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -21,28 +20,30 @@ fun CustumeTopAppBar(
     canNavigateBack: Boolean,
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
-    navigateUp:() -> Unit = {},
+    navigateUp: () -> Unit = {},
     onRefresh: () -> Unit = {},
 ) {
     CenterAlignedTopAppBar(
         title = { Text(title) },
-        actions = {
-            Icon(
-                imageVector = Icons.Default.Refresh,
-                contentDescription = " ",
-                modifier = Modifier.clickable {
-                    onRefresh()
-                })
-        },
-        modifier = modifier,
-        scrollBehavior = scrollBehavior, navigationIcon = {
-            if(canNavigateBack) {
+        navigationIcon = {
+            if (canNavigateBack) {
                 IconButton(onClick = navigateUp) {
-                    Icon(imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = null)
-
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
                 }
             }
-        }
+        },
+        actions = {
+            IconButton(onClick = onRefresh) {
+                Icon(
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = "Refresh"
+                )
+            }
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior
     )
 }
