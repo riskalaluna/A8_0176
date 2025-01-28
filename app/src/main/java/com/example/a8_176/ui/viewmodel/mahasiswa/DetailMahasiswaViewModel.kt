b5cd4b0507fffcf0bd508db4ef49af3b44c34b00
riskalaluna <riskalaluna@gmail.com>
2025-01-28 21:07:17 +0700

    DetailMahasiswaViewModel mengelola pengambilan data detail bangunan dari repository mahasiswa

--- app/src/main/java/com/example/a8_176/ui/viewmodel/mahasiswa/DetailMahasiswaViewModel.kt
+++ app/src/main/java/com/example/a8_176/ui/viewmodel/mahasiswa/DetailMahasiswaViewModel.kt
@@ -1,6 +1,34 @@
 package com.example.a8_176.ui.viewmodel.mahasiswa
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.a8_176.model.Mahasiswa
+import com.example.a8_176.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class DetailMahasiswaViewModel(
+    private val mhsRepository: MahasiswaRepository
+) : ViewModel() {
+    var uiState by mutableStateOf(DetailUiState())
+        private set
+
+    fun fetchDetailMahasiswa(id_mahasiswa: String) {
+        viewModelScope.launch {
+            uiState = DetailUiState(isLoading = true)
+            try {
+                val mahasiswa = mhsRepository.getMahasiswabyId(id_mahasiswa)
+                uiState = DetailUiState(detailUiEvent = mahasiswa.toDetailUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                uiState = DetailUiState(isError = true, errorMessage = "Failed to fetch details: ${e.message}")
+            }
+        }
+    }
+}
 
 data class DetailUiState(
     val detailUiEvent: InsertMhsUiEvent = InsertMhsUiEvent(),
