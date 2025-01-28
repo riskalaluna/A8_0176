745d95f70f5ae13ca5f38202df98d35bcf01f271
riskalaluna <riskalaluna@gmail.com>
2025-01-28 16:28:30 +0700

    Menambahkan beberapa code untuk memperindah tampilan

--- app/src/main/java/com/example/a8_176/ui/custumwidget/TopAppBar.kt
+++ app/src/main/java/com/example/a8_176/ui/custumwidget/TopAppBar.kt
@@ -1,6 +1,11 @@
 package com.example.a8_176.ui.custumwidget
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
@@ -8,10 +13,20 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlurEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.a8_176.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,27 +38,43 @@ fun CustumeTopAppBar(
     navigateUp: () -> Unit = {},
     onRefresh: () -> Unit = {},
 ) {
-    CenterAlignedTopAppBar(
-        title = { Text(title) },
-        navigationIcon = {
-            if (canNavigateBack) {
-                IconButton(onClick = navigateUp) {
+    Column(
+        modifier = modifier
+    ) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF1976D2),
+                    fontSize = 27.sp
+                )
+            },
+            navigationIcon = {
+                if (canNavigateBack) {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.Blue
+                        )
+                    }
+                }
+            },
+            actions = {
+                IconButton(onClick = onRefresh) {
                     Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = "Back"
+                        imageVector = Icons.Default.Refresh,
+                        contentDescription = "Refresh",
+                        tint = Color.Blue
                     )
                 }
-            }
-        },
-        actions = {
-            IconButton(onClick = onRefresh) {
-                Icon(
-                    imageVector = Icons.Default.Refresh,
-                    contentDescription = "Refresh"
-                )
-            }
-        },
-        modifier = modifier,
-        scrollBehavior = scrollBehavior
-    )
+            },
+            modifier = modifier,
+            scrollBehavior = scrollBehavior,
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = Color.Transparent
+            )
+        )
+    }
 }
