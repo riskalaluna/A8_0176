ef292169ce503fb67da06add21745f41cb9779de
riskalaluna <riskalaluna@gmail.com>
2025-01-28 21:39:27 +0700

    DestinasiUpdateMhs menyediakan informasi rute navigasi untuk layar update mahasiswa

--- app/src/main/java/com/example/a8_176/ui/view/mahasiswa/UpdateMahasiswaView.kt
+++ app/src/main/java/com/example/a8_176/ui/view/mahasiswa/UpdateMahasiswaView.kt
@@ -10,12 +10,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a8_176.ui.custumwidget.CustumeTopAppBar
+import com.example.a8_176.ui.navigation.DestinasiNavigasi
+import com.example.a8_176.ui.view.kamar.EntryBodyKmr
 import com.example.a8_176.ui.viewmodel.PenyediaViewModel
 import com.example.a8_176.ui.viewmodel.mahasiswa.UpdateMahasiswaViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+
+object DestinasiUpdateMhs: DestinasiNavigasi {
+    override val route = "update/{id_mahasiswa}"
+    override val titleRes = "Update Mahasiswa"
+    const val id_mahasiswa = "id_mahasiswa"
+    val routeWithArgs = "$route/{$id_mahasiswa}"
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
