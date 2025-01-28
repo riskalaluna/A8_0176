3162d1273b42e0e2bc97f3aa79a662a9bef9653c
riskalaluna <riskalaluna@gmail.com>
2025-01-28 21:50:40 +0700

    menangani operasi insert data Pembayaran dan mengambil id_mahasiswa dari tabel mahasiswa

--- app/src/main/java/com/example/a8_176/ui/viewmodel/pembayaransewa/InsertPembayaranSewaViewModel.kt
+++ app/src/main/java/com/example/a8_176/ui/viewmodel/pembayaransewa/InsertPembayaranSewaViewModel.kt
@@ -1,7 +1,53 @@
 package com.example.a8_176.ui.viewmodel.pembayaransewa
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.a8_176.model.Mahasiswa
 import com.example.a8_176.model.PembayaranSewa
+import com.example.a8_176.repository.MahasiswaRepository
+import com.example.a8_176.repository.PembayaransewaRepository
+import kotlinx.coroutines.launch
+
+class InsertPembayaranSewaViewModel(
+    private val ps: PembayaransewaRepository,
+    private val mhs: MahasiswaRepository
+) : ViewModel() {
+    var uiStatePembayaranSewa by mutableStateOf(InsertPsUiState())
+        private set
+
+    fun updateInsertPsState(insertPsUiEvent: InsertPsUiEvent) {
+        uiStatePembayaranSewa = uiStatePembayaranSewa.copy(insertPsUiEvent = insertPsUiEvent)
+    }
+
+    init {
+        loadMahasiswa()
+    }
+
+    private fun loadMahasiswa() {
+        viewModelScope.launch {
+            try {
+                val mahasiswaList = mhs.getMahasiswa()
+                uiStatePembayaranSewa = uiStatePembayaranSewa.copy(idmahasiswaList = mahasiswaList)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    // Fungsi untuk menyimpan data pembayaran sewa ke repository
+    fun insertPs() {
+        viewModelScope.launch {
+            try {
+                ps.insertPembayaransewa(uiStatePembayaranSewa.insertPsUiEvent.toPs())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class InsertPsUiState(
     val insertPsUiEvent: InsertPsUiEvent = InsertPsUiEvent(),
