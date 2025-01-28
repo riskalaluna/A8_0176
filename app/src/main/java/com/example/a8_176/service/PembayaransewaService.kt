368a9621479aa7440bd8433067bdfbcfb5cd7919
riskalaluna <riskalaluna@gmail.com>
2025-01-28 19:43:08 +0700

    memperbaiki code dan menambahkan pembayaran/ agar bisa melaukan operasi CRUD

--- app/src/main/java/com/example/a8_176/service/PembayaransewaService.kt
+++ app/src/main/java/com/example/a8_176/service/PembayaransewaService.kt
@@ -15,26 +15,24 @@ import retrofit2.http.Path
 interface PembayaransewaService {
     @Headers(
         "Accept: application/json",
-        "Content-Type: application/json",
+        "Content-Type: application/json"
     )
 
-    //@GET("bacapembayaransewa.php")
-    @GET(".")
+    @GET("pembayaransewa")
     suspend fun getAllPembayaransewa(): AllPembayaransewaResponse
 
-    //@GET("baca1pembayaransewa.php/{id_pembayaran}")
-    @GET("{id_pembayaran}")
+    @GET("pembayaransewa/{id_pembayaran}")
     suspend fun getPembayaransewabyId(@Path("id_pembayaran") id_pembayaran: String): PembayaransewaDetailResponse
 
-    @POST("store")
+    @POST("pembayaransewa/store")
     suspend fun insertPembayaransewa(@Body pembayaranSewa: PembayaranSewa)
 
-    //@PUT("editpembayaransewa.php/{id_pembayaran}")
-    @PUT("{id_pembayaran}")
-    suspend fun updatePembayaransewa(@Path("id_pembayaran") id_pembayaran: String, @Body pembayaranSewa: PembayaranSewa)
+    @PUT("pembayaransewa/{id_pembayaran}")
+    suspend fun updatePembayaransewa(
+        @Path("id_pembayaran") id_pembayaran: String,
+        @Body pembayaranSewa: PembayaranSewa
+    ): Response<PembayaranSewa>
 
-    //@DELETE("deletepembayaransewa.php/{id_pembayaran}")
-    @DELETE("{id_pembayaran}")
-    suspend fun deletePembayaransewa (@Path("id_pembayaran") id_pembayaran: String): Response<Void>
-
+    @DELETE("pembayaransewa/{id_pembayaran}")
+    suspend fun deletePembayaransewa(@Path("id_pembayaran") id_pembayaran: String): Response<Void>
 }
