9c5066aba34bbd5f18e3c053a053f44b9c4c9ee8
riskalaluna <riskalaluna@gmail.com>
2025-01-28 15:18:43 +0700

    mengelola pengambilan data detail kamar dari repository

--- app/src/main/java/com/example/a8_176/ui/viewmodel/kamar/DetailKamarViewModel.kt
+++ app/src/main/java/com/example/a8_176/ui/viewmodel/kamar/DetailKamarViewModel.kt
@@ -1,6 +1,33 @@
 package com.example.a8_176.ui.viewmodel.kamar
 
 import com.example.a8_176.model.Kamar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a8_176.repository.KamarRepository
+import kotlinx.coroutines.launch
+
+class DetailKamarViewModel(
+    private val kmrRepository: KamarRepository
+) : ViewModel() {
+    var uiState by mutableStateOf(DetailKmrUiState())
+        private set
+
+    fun fetchDetailKamar(id_kamar: String) {
+        viewModelScope.launch {
+            uiState = DetailKmrUiState(isLoading = true)
+            try {
+                val kamar = kmrRepository.getKamarbyId(id_kamar)
+                uiState = DetailKmrUiState(detailKmrUiEvent = kamar.toDetailKmrUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                uiState = DetailKmrUiState(isError = true, errorMessage = "Failed to fetch details: ${e.message}")
+            }
+        }
+    }
+}
 
 data class DetailKmrUiState(
     val detailKmrUiEvent: InsertKmrUiEvent = InsertKmrUiEvent(),
